65590a0c47d970eca8ede6ebe1b66060dde166d7
Rui Fernandes <ruiwiebbeling@gmail.com>
2024-07-26 22:13:54 -0300

    Update User.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-// para o TypeORM rodar no TypeScript sem error, altere a opção "strictPropertyInitialization" em 'tsconfig.json' para FALSE
+// para o TypeORM rodar no TypeScript sem erro, altere a opção "strictPropertyInitialization" em 'tsconfig.json' para FALSE
 
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
